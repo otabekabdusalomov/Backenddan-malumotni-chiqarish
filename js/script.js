aa06789bc2f1b409f616fd74bc3de7f69969ea73
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2022-07-02 17:28:02 +0300

    movies api

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 "use strict";
 
-const API_KEY = '89f0c884';
+const API_KEY = 'b1566df1';
+const search = "hulk";
+const page = 1;
 
+const getMovies = async function() {
+    const request = await fetch(
+
+        `https://omdbapi.com/?apikey=${API_KEY}b1566df1&s=${search}&page=${page}`
+        );
+
+
+
+    const data = await request.json();
+    console.log(data.Search);
+};
+
+getMovies();
