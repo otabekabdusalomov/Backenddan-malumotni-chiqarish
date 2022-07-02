23ee811d3ed48c3e3b47e84c44beddbf5fb44972
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2022-07-02 13:29:21 +0300

    selector

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 "use strict";
+const elList = document.querySelector(".list");
 
-const response = fetch("https://jsonplaceholder.typicode.com/users/").then
-(response => response.json()).then(data => console.log((data)));
 
-console.log(response);
+const renderUsers = function (arr, htmlElement) {
+    setTimeout(function () {
+      if (arr.length > 0) {
+        elList.innerHTML = null;
+  
+        arr.forEach((item) => {
+          const newLi = document.createElement("li");
+  
+          newLi.textContent = item.name;
+  
+          htmlElement.appendChild(newLi);
+        });
+      }
+    }, 1000);
+  };
+
+
+const response = fetch("https://jsonplaceholder.typicode.com/users/")
+.then(response => response.json())
+.then(data => renderUsers(data, elList));
